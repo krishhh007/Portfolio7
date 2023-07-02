cc09102e46e73ca2fcb0694197827aaef698de73
Krishnaraj S <96368336+Krishnaraj017@users.noreply.github.com>
2023-07-02 17:06:19 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { FaHandPaper } from "react-icons/fa"
 const About = () => {
     const [heading] = useState<string>('hello everyone');
-    const [description] = useState<string>('I\'m Rohit Kumar Mandal, a talented Frontend Developer and UI Designer with a passion for creating exceptional web experiences. With expertise in HTML, CSS, Bootstrap , Tailwincss , javascript , Figma , React , Nextjs , Nodejs , Php , MySQL , Github , Netlify , Midjourney .  I combine my technical skills with a keen eye for aesthetics to craft visually appealing and user-friendly interfaces.');
+    const [description] = useState<string>('I\'m Krishnaraj S, a talented Frontend Developer and UI Designer with a passion for creating exceptional web experiences. With expertise in HTML, CSS, Bootstrap , Tailwincss , javascript , Figma , React , Nextjs , Nodejs , Php , MySQL , Github , Netlify , Midjourney .  I combine my technical skills with a keen eye for aesthetics to craft visually appealing and user-friendly interfaces.');
   return (
     <div className="py-6 border-b border-zinc-700">
     <h3 className="text-gray-400 capitalize text-lg font-medium">about me</h3>
