bda5c862aad6dfc9fabd1153ce86e50a5ab24085
Krishnaraj S <96368336+Krishnaraj017@users.noreply.github.com>
2023-07-02 17:22:33 +0530

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,32 +2,45 @@ import Image from "next/image";
 import { useState } from "react"
 const Skills = () => {
     const [skillsList] = useState([
-        {id: 1, name: 'HTML', image: '/images/html.png'},
-        {id: 2, name: 'CSS', image: '/images/css.png'},
-       // {id: 3, name: 'sass', image: '/images/sass.png'},
-        {id: 4, name: 'bootstrap', image: '/images/bootstrap.png'},
-        {id: 5, name: 'tailwindcss', image: '/images/tailwind.png'},
-        {id: 6, name: 'JavaScript', image: '/images/javascript.png'},
-        {id: 7, name: 'figma', image: '/images/figma.png'},
-        {id: 8, name: 'react', image: '/images/react.png'},
-      //  {id: 9, name: 'redux', image: '/images/redux.png'},
-       // {id: 10, name: 'styled components', image: '/images/styled.png'},
-        {id: 11, name: 'next', image: '/images/next-js.png'},
-      //  {id: 12, name: 'vue', image: '/images/vue.png'},
-      //  {id: 345345, name: 'vuex', image: '/images/vuex.png'},
-        {id: 13, name: 'node', image: '/images/node.png'},
-      //  {id: 14, name: 'express', image: '/images/express.png'},
-      //  {id: 15, name: 'mongodb', image: '/images/mongodb.png'},
-        {id: 16, name: 'PHP', image: '/images/php.png'},
-        {id: 17, name: 'MySQL', image: '/images/mysql.png'},
-      //  {id: 18, name: 'solidity', image: '/images/sol.png'},
-        // {id: 343453, name: 'ethereum', image: '/images/eth.png'},
-        {id: 19, name: 'GitHub', image: '/images/git.png'},
-      //  {id: 20, name: 'jest', image: '/images/jest.png'},
-        // {id: 21, name: 'AWS Amplify', image: '/images/aws.png'},
-       // {id: 22, name: 'heroku', image: '/images/heroku.png'},
-        {id: 23, name: 'netlify', image: '/images/netlify.png'},
-       // {id: 24, name: 'third web', image: '/images/thirdweb.png'},
+ { id: 1, name: "HTML", image: "/images/html.png" },
+      { id: 2, name: "CSS", image: "/images/css.png" },
+      { id: 3, name: "Flutter", image: "/images/flutter.png" },
+      { id: 4, name: "react native", image: "/images/react.png" },
+      {
+        id: 5,
+        name: "Android Studio",
+
+        image: "/images/icons8-android-studio-48.png",
+      },
+      
+
+      { id: 6, name: "figma", image: "/images/figma.png" },
+      
+      { id:7, name: "node", image: "/images/node.png" },
+      { id: 8, name: "express", image: "/images/express.png" },
+      
+      {
+        id: 9,
+        name: "Firebase",
+        image: "/images/icons8-firebase-48.png",
+      },
+      { id: 10, name: "MySQL", image: "/images/mysql.png" },
+    
+      { id: 11, name: "Jira", image: "/images/icons8-jira-48.png" },
+        {id: 12, name: 'Agile Methodology', image: '/images/icons8-agile-64.png'},
+      
+      { id: 13, name: "Dart", image: "/images/icons8-dart-48.png" },
+      {
+        id: 14,
+        name: "Java",
+        image: "/images/icons8-java-64.png",
+      },
+      { id: 15, name: "JavaScript", image: "/images/javascript.png" },
+      { id: 16, name: "Python", image: "/images/python.png" },
+
+      { id: 17, name: "C", image: "/images/icons8-c-64.png" },
+      
+      { id: 18, name: "netlify", image: "/images/netlify.png" },
     ]);
   return (
     <div className="py-6 border-b border-zinc-700">
