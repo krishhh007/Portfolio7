ea8cd39854f8cd75f8d3c7a5367bc1cad407eddd
Krishnaraj S <96368336+Krishnaraj017@users.noreply.github.com>
2023-07-02 17:26:03 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <About />
         <Skills />
         <Positions />
-{/*         <Projects /> */}
+        <Position1/>
         <Footer />
       </div>
     </div>
