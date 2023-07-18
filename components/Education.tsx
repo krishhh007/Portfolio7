1924341d971db1287dbe8d42aa641531646e073b
Krishnaraj S <96368336+Krishnaraj017@users.noreply.github.com>
2023-07-18 17:32:42 +0530

    Update Education.tsx

--- components/Education.tsx
+++ components/Education.tsx
@@ -33,7 +33,7 @@ const Education = () => {
        options: [
       "",
     
-      // Add more options as needed
+      // Add more
     ],
     },
 
@@ -44,7 +44,7 @@ const Education = () => {
       type: "S.R Public School-CBSE,hebri.",
       date: "2016-2017",
        skills: [
-      { id: 1, image: "/images/icons8-school-64.png", name: "CBSE" }
+      { id: 1, image: "/images/cbse.png", name: "CBSE" }
       
       
     ],
