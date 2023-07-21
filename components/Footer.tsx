908d215292fd302217a93079f4920b0830c4534c
Krishnaraj S <96368336+Krishnaraj017@users.noreply.github.com>
2023-07-22 00:14:06 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="text-gray-300 bg-zinc-800/50 py-3 px-5 rounded-tl rounded-tr">
        <div className="max-w-screen-lg w-full mx-auto flex items-center space-x-2">
-       <span className="text-sm font-medium">Maintained by</span><span className="text-gray-500 font-semibold">Krishnaraj Shetty🎶</span>
+       <span className="text-sm font-medium">Maintained by</span><span className="text-gray-500 font-semibold">Krishnaraj Shetty</span>
         <span className="text-sm">&copy;</span>
         <span className="text-sm">{new Date().getFullYear()}</span>
        </div>
